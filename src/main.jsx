4307bf9cadc36b5773f74ad633c0094a86ed69b8
masinlocandher-max <masinlocandher@gmail.com>
2026-09-05 20:58:45 +0800

    Load first-run join styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './styles.css';
 import './home.css';
 import './showcase.css';
+import './join.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
